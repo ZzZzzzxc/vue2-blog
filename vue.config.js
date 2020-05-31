b46feb1f2c8b2b20f23b426ef47b06adced88ca1
ZxcCC <731458684@qq.com>
2020-05-31 10:40:57 +0800

    feat:生产环境CDN

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,27 @@ const createThemeColorReplacerPlugin = require("./config/plugin.config");
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
+
+const isProd = process.env.NODE_ENV === "production";
+
+const assetsCDN = {
+  // webpack build externals
+  externals: {
+    vue: "Vue",
+    "vue-router": "VueRouter",
+    vuex: "Vuex",
+    axios: "axios",
+  },
+  css: [],
+  // https://unpkg.com/browse/vue@2.6.10/
+  js: [
+    "//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js",
+    "//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js",
+    "//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js",
+    "//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js",
+  ],
+};
+
 // vue.config.js
 const vueConfig = {
   configureWebpack: {
@@ -13,6 +34,7 @@ const vueConfig = {
       // Ignore all locale files of moment.js
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     ],
+    externals: isProd ? assetsCDN.externals : {},
   },
 
   chainWebpack: (config) => {
